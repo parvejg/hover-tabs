fee300b6c237f39fbb9124cac514de8241a0c9f2
parvejg <parvezpg321@gmail.com>
2023-11-22 19:16:46 +0530

    complete link work and css work

--- src/Components/HoverTabs.js
+++ src/Components/HoverTabs.js
@@ -1,15 +1,15 @@
 import "./HoverTabs.css";
 export const HoverTabs = () => {
   return (
-    <>
+    <div className="main-container">
       <div className="hover-tabs-wrapper">
         <div className="hover-tabs-links-wrapper">
-          <button>London</button>
-          <button>Paris</button>
-          <button>Tokyo</button>
+          <button className="hover-tabs-links">London</button>
+          <button className="hover-tabs-links">Paris</button>
+          <button className="hover-tabs-links">Tokyo</button>
         </div>
       </div>
       <div className="tabs-links-contents"></div>
-    </>
+    </div>
   );
 };
